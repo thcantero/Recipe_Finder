ee6680f11212e6e18c67a7f9f953117f4f8ad0fe
Thalia Cantero <thal.cantero@gmail.com>
2025-04-13 22:58:28 -0400

    Moved db to Supabase and prepared for deploying in Render

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ load_dotenv()
 SPOONACULAR_API_KEY = os.environ.get('SPOONACULAR_API_KEY')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///recipefinder'
+app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL','postgresql:///recipefinder'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
